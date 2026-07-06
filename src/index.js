b090b7b4fbc3a95311d797bbe8e342358f525eea
Raushan Kumar <raushan76594@gmail.com>
2026-07-06 10:46:00 +0530

    mongoDB Atlas connection

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import express from 'express'
 import dotenv from "dotenv";
+import connect_DB from './db/index.js';
 
 dotenv.config({
-  path: './env'
+  path: './.env'
 });
 
 const app = express()
 
-//immediately invoked function expression (IIFE)
-//syntax : ;()()
-// ; meams start of a new statement and ()() mean IIFE
-;(async() => {
-  try {
-    console.log(process.env.MONGODB_URL);
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    app.on("error", (error) => {
-      console.error("Error : ",error)
-      throw error
-    })
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`)
-    })
-  } catch (error) {
-    console.error("Error : ",error)
-    throw error
-  }
-})()
+connect_DB()
+ 
