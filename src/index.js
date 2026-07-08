ca2a745112ec93033155cfe5542e9125914a9cc2
Raushan Kumar <raushan76594@gmail.com>
2026-07-08 11:21:08 +0530

    ApiError and ApiResponse standardization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import express from 'express'
 import dotenv from "dotenv";
 import connect_DB from './db/index.js';
 
 dotenv.config({
   path: './.env'
 });
-
-const app = express()
 
 connect_DB()
  
